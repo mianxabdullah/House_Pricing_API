f60b5a76c819f29e9bd8fe65cc04bf6e7832eabe
mianxabdullah <mian.abdullah.ayub@gmail.com>
2026-07-26 01:27:18 +0500

    loaded schema and added home() endpoint

--- app/main.py
+++ app/main.py
@@ -13,3 +13,13 @@ app = FastAPI(
 model = joblib.load('../model/house_model.joblib')
 features = joblib.load('../model/house_features.joblib')
 
+from app.schemas import HouseData
+
+@app.get("/")
+def home():
+    return {"message": "Welcome to the House Price Prediction API!",
+            "status": "Active",
+            "version": "1.0.0",
+            "endpoints": "Send post request to /predict : POST endpoint to predict house prices based on input features"
+            }
+
